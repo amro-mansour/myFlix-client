17e055ce586c3c65471e0fba973c8e692b64dd50
amro-mansour <amro8000@gmail.com>
2022-05-12 10:09:19 +0200

    Deleting the in memory movies array

--- src/index.jsx
+++ src/index.jsx
@@ -7,18 +7,6 @@ import './index.scss';
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      movies: [
-        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...' },
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...' },
-        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...' }
-      ]
-    }
-  }
-
   render() {
     return (
       <MainView />
