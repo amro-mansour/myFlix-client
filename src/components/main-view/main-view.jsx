6c11d93f064ec19ef61266c041865e633036c0d3
amro-mansour <amro8000@gmail.com>
2022-05-19 12:10:37 +0200

    Adding the navbar-view component to the main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import axios from 'axios';
-
+import { Row, Col, Container } from 'react-bootstrap';
+import { NavbarView } from '../navbar-view/navbar-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { Navbar, Nav } from 'react-bootstrap';
 
 class MainView extends React.Component {
 
@@ -62,15 +64,25 @@ class MainView extends React.Component {
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
-      <div className="main-view">
-        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
-        {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
-          ))
-        }
-      </div>
+      <Container>
+        <Row>
+          <NavbarView />
+        </Row>
+        <Row className="main-view justify-content-md-center">
+          {selectedMovie
+            ? (
+              <Col md={8}>
+                <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+              </Col>
+            )
+            : movies.map(movie => (
+              <Col sm={6} md={4}>
+                <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+              </Col>
+            ))
+          }
+        </Row>
+      </Container>
     );
   }
 }
