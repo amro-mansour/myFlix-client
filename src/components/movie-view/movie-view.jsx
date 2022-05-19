e5ab5ffe8ed1e66efa6364db5e331b19c35f6b44
amro-mansour <amro8000@gmail.com>
2022-05-19 12:05:24 +0200

    Adding a Bootstrap Card element to the movie-view component

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './movie-view.scss';
+import { Card, Col, Container, Row, Button } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
 
@@ -7,30 +9,29 @@ export class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.Name}</span>
-        </div>
-        <button onClick={() => { onBackClick(null) }}>Back</button>
 
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <Card className="movie-view__card" style={{ width: '40rem' }}>
+              <Card.Body>
+                <Card.Img className="movie-view__image" variant="top" src={movie.ImagePath} />
+                <Card.Title className="title-style">{movie.Title}</Card.Title>
+                <Card.Text className="text-style">Genre: {movie.Genre.Name}</Card.Text>
+                <Card.Text className="text-style">Director: {movie.Director.Name}</Card.Text>
+                <Card.Text className="text-style">{movie.Description}</Card.Text>
+                <Button variant="outline-warning" onClick={() => { onBackClick(null) }}>Back</Button>
+                <Button className="add-list__button" variant="warning">Add to your list</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
 
-MovieView.PropTypes = {
+MovieView.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
