2be554ad331ade8dc6579c18714cd475240ee9fb
amro-mansour <amro8000@gmail.com>
2022-05-19 20:06:20 +0200

    Adding the id attribute name to render the app correctly

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainView from './components/main-view/main-view';
-import { NavbarView } from './components/navbar-view/navbar-view';
 import Container from 'react-bootstrap/Container';
-import { Navbar, Nav } from 'react-bootstrap';
+import { createRoot } from "react-dom/client";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
+
   render() {
     return (
       <Container>
         <MainView />
       </Container>
-    );
+    )
   }
 }
 
-// Finds the root of your app
-const container = document.getElementsByClassName('app-container')[0];
-
-// Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+const container = document.getElementById('app-container');
+const root = createRoot(container);
+root.render(<MyFlixApplication />);
