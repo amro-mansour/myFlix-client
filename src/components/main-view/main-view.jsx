8c42a1ccb17da9aeaaf79a12c615681ec076b5e7
amro-mansour <amro8000@gmail.com>
2022-05-19 20:02:46 +0200

    moving the key id to the column
    moving the key id to the column to avoid getting errors with movies.map in the console

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -76,8 +76,8 @@ class MainView extends React.Component {
               </Col>
             )
             : movies.map(movie => (
-              <Col sm={6} md={4}>
-                <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+              <Col key={movie._id} sm={6} md={4}>
+                <MovieCard movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
               </Col>
             ))
           }
