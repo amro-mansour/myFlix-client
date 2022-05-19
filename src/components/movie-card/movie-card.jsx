8e580bea21ee9c2ab29398be5366e8e9e6f9b87c
amro-mansour <amro8000@gmail.com>
2022-05-19 12:04:19 +0200

    Adding a Bootstrap card element to the movie-card component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './movie-card.scss';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
 
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+    return (
+      <Card className="card-style" style={{ width: '18rem' }}>
+        <Card.Img variant="top" className="image-style" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title className="title-style">{movie.Title}</Card.Title>
+          <Card.Text className="text-style">{movie.Description}</Card.Text>
+          <Button className="button-style" onClick={() => onMovieClick(movie)} variant="warning">Open</Button>
+        </Card.Body>
+      </Card>
+    );
   }
 }
 
