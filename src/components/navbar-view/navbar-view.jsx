ee1c159aecf0bc91225f9eb0f4f4d403eff89414
amro-mansour <amro8000@gmail.com>
2022-05-28 12:48:27 +0200

    adding a navigation bar

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -1,26 +1,61 @@
 import React from "react";
 import './navbar-view.scss';
-
 import { Navbar, Container, Nav, Button, Offcanvas, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
-export class NavbarView extends React.Component {
+export function NavbarView({ user }) {
 
-  render() {
-
-    return (
-
-      <Container>
-        <Navbar bg="light" variant="light">
-          <Container className="navbar-container">
-            <Navbar.Brand href="#home">MyFlix-App</Navbar.Brand>
-            <Nav className="me-auto navbar-elements__style">
-              <Nav.Link className="navbar-element" href="#home">Home</Nav.Link>
-              <Nav.Link className="navbar-element" href="#features">My-Movies</Nav.Link>
-              <Nav.Link className="navbar-element" href="#pricing">Account</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-      </Container>
-    );
+  const onLoggedOut = () => {
+    localStorage.clear();
+    window.open("/", "_self");
   }
+
+  const isAuth = () => {
+    if (typeof window == "undefined") {
+      return false
+    }
+    if (localStorage.getItem("token")) {
+      return localStorage.getItem("token");
+    } else {
+      return false;
+    }
+  };
+
+  return (
+    <Container>
+      <Navbar bg="light" expand="lg">
+        <Container className="navbar-container">
+          <Navbar.Brand as={Link} to={"/"} href="#home">MyFlix-App</Navbar.Brand>
+
+          <Nav className="me-auto navbar-elements__style">
+
+            {isAuth() && (
+              <Nav.Link as={Link} to={`/`}>Movies</Nav.Link>
+            )}
+
+            {isAuth() && (
+              <Nav.Link as={Link} to={`/users/${user}`}>Profile</Nav.Link>
+            )}
+
+            {isAuth() && (
+              <Nav.Link onClick={() => onLoggedOut()}>Logout</Nav.Link>
+            )}
+
+            {!isAuth() && (
+              <Nav.Link as={Link} to={`/`}>Login</Nav.Link>
+            )}
+
+            {!isAuth() && (
+              <Nav.Link as={Link} to={`/register`}>Sign Up</Nav.Link>
+            )}
+
+
+          </Nav>
+
+        </Container>
+      </Navbar>
+    </Container>
+
+  )
 }
+
